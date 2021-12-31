293ebbbf7b0a70b997026175ddb22c3484a297dd
YellowPrawn <damienfung616@gmail.com>
2021-12-31 12:29:30 -0800

    Implemented frame quadrilateral detection

---
+++ ObjectTracker/CourtTemplateMatching.py
@@ -0,0 +1,99 @@
+import cv2
+
+
+def readFrame():
+    """ Read a frame from given video.
+
+    Loads frame into program as an RGB image and rescales it
+
+    Returns:
+         A cv2 image corresponding to the read frame
+    """
+    # reading frame and rescaling
+    scale = 1 / 4
+
+    frame = cv2.imread("test.png", cv2.IMREAD_COLOR)
+    (HEIGHT, WIDTH) = frame.shape[:2]
+    frameRGB = cv2.resize(frame, (int(WIDTH * scale), int(HEIGHT * scale)))
+
+    return frameRGB
+
+
+def processContours():
+    """ Processes contours in given frame
+
+    Finds all contours in frame
+
+    Returns:
+            A list of cv2 contours
+    """
+    img = readFrame()
+
+    # converting frames to grayscale
+    frameGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # contour detection
+    ret, thresh = cv2.threshold(frameGrey, 127, 255, 0)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    return contours
+
+
+def extractQuadrilaterals():
+    """ Finds all quadrilateral contours
+
+    determines if a contour is a quadrilateral. If a quadrilateral is found, adds it to list "quadrilaterals"
+
+    Returns:
+         A list of contours, and a cv2 image with all quadrilateral contours in the frame as an overlay
+    """
+    i = 0
+    img = readFrame()
+    quadrilaterals = []
+
+    for contour in processContours():
+        # here we are ignoring first counter because findContours function detects whole image as shape
+        if i == 0:
+            i = 1
+            continue
+
+        # cv2.approxPloyDP() function to approximate the shape
+        approx = cv2.approxPolyDP(
+            contour, 0.01 * cv2.arcLength(contour, True), True)
+
+        # detect quadrilaterals (to find the 4 quadrilaterals that are used to define the court's bounds) and draw
+        if len(approx) == 4:
+            cv2.drawContours(img, [contour], -1, (0, 255, 0), 1, lineType=cv2.LINE_AA)
+            quadrilaterals.append(contour)
+    return quadrilaterals, img
+
+
+def courtBoundingBox():
+    """ Finds bounds of badminton court
+
+    Using quadrilaterals in frame, determine bounds of the court by searching for at least 2 matching squares which
+    define corners of the court. Other bounds can be determined by propagating lines according to official badminton
+    court guidelines
+
+    Returns:
+         court bounding box and a cv2 image with bounding quadrilateral contours in the frame as an overlay
+    """
+    quadrilaterals, img = extractQuadrilaterals()
+    # TODO: implement quadrilateral matching to determine bounds of court
+    return img
+
+
+def transformFrame():
+    """ standardizes court perspective
+
+    Using the bounding quadrilaterals, shift perspective into "bird's eye view" by transforming bounding quadrilaterals
+    into rectangles and stretch image into standard badminton court dimensions.
+
+    Returns:
+        cv2 image of the court.
+    """
+
+
+cv2.imshow("display", courtBoundingBox())
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Badminton-Match-Analysis
-Analyzing professional badminton matches using Pose Recognition
+# Badminton Match Analysis
+
+The objective of this project is to detect the most common form of winning a badminton match in the 2020 Olympics.
+
+The ways a player can lose a round is through the following:
+
+- In-court loss (IN)
+- Out-of-court loss (OUT)
+- Other; all of which will be considered as fouls for simplicity (FOUL) 
